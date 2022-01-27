883239eae6bbd583224240afa02a1c1e68b61871
yeongjong <tohero21@naver.com>
2022-01-28 02:39:14 +0900

    Add - size에 css unit 입력가능하도록 수정

--- src/Components/Grid/Grid.jsx
+++ src/Components/Grid/Grid.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import styles from './Grid.module.css';
 import { COLUMNS } from './constants';
-import { classNames } from 'utils';
+import { classNames, isCssUnit } from 'utils';
 
-export default function Grid({ className, container = false, xs, item = true, children }) {
-  const gridWidth = (xs / COLUMNS) * 100 + '%';
+export default function Grid({ className, container = false, size, item = true, children }) {
+  const gridWidth = isCssUnit(size) ? size : (size / COLUMNS) * 100 + '%';
 
   return (
     <div
       className={classNames(container ? styles.gridContainer : styles.gridItem, className)}
       {...{
-        style: xs && {
+        style: size && {
           width: gridWidth,
           flexBasis: gridWidth,
           flexGrow: 0,
