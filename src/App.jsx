ea3083ce99ff5e2f76954474127c4bf2c18e402b
yeongjong <tohero21@naver.com>
2022-01-28 03:56:57 +0900

    Add - App Menu, Contents 영역 분리

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
-import { BasicInfo } from 'Components';
+import { BasicInfo, Grid } from 'Components';
+import styles from './App.module.css';
+
+const MenuWidth = 300;
 
 const App = () => {
   return (
-    <div>
-      <BasicInfo />
-    </div>
+    <main className={styles.main}>
+      <Grid container>
+        <Grid item size={`${MenuWidth}px`}>
+          Menu
+        </Grid>
+        <Grid container size={`calc(100% - ${MenuWidth}px)`}>
+          <Grid item>
+            <BasicInfo />
+          </Grid>
+        </Grid>
+      </Grid>
+    </main>
   );
 };
 
