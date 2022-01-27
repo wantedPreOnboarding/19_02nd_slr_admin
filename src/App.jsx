1f2224b7417c9adda5a064a6b7094ce50385dcb4
yeongjong <tohero21@naver.com>
2022-01-28 00:43:20 +0900

    Merge upstream to origin

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
-
+<<<<<<< HEAD
+=======
+import ProductOption from 'Components/ProductOption/ProductOption';
+>>>>>>> e5f25f52c52277a7ce37fee2e81fd79a796620c1
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <ProductOption />
+    </div>
+  );
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'styles/index.module.css';
+<<<<<<< HEAD
+<<<<<<< HEAD
 import 'styles/theme.module.css';
+=======
+
+>>>>>>> e5f25f52c52277a7ce37fee2e81fd79a796620c1
+=======
+
+>>>>>>> e5f25f52c52277a7ce37fee2e81fd79a796620c1
 ReactDOM.render(
   <React.StrictMode>
     <App />
