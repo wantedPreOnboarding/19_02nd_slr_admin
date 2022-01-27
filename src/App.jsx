78ae42dce70417cb1d21921d58c7486fc302309b
hyun <vi2920va@gmail.com>
2022-01-27 19:06:55 +0900

    Add - Aside 컴포넌트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import Aside from 'components/Aside/Aside';
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Aside />
+    </div>);
 };
 
 export default App;
