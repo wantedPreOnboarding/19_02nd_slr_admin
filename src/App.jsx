08afea3e394b6e9b2f4f78885646fb79e345e31f
hyun <vi2920va@gmail.com>
2022-01-27 21:59:58 +0900

    Add - 상품 소개 이미지, 구매자 추천 이미지 컴포넌트 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import Aside from 'Components/Aside/Aside';
+import ProductIntroImg from 'Components/ProductIntroImg/ProductIntroImg';
+import BuyerRecommendImg from 'Components/BuyerRecommendImg/BuyerRecommendImg';
 
 const App = () => {
   return (
     <div>
       <Aside />
-    </div>);
+      <ProductIntroImg />
+      <BuyerRecommendImg />
+    </div>
+  );
 };
 
 export default App;
