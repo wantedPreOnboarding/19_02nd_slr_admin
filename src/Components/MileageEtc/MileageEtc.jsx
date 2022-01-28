ee17d6972a56b448ab6731c68bc17384fadf4fef
Da Bin Jeong <90027202+b41-41@users.noreply.github.com>
2022-01-29 05:52:32 +0900

    Update MileageEtc.jsx

--- src/Components/MileageEtc/MileageEtc.jsx
+++ src/Components/MileageEtc/MileageEtc.jsx
@@ -1,29 +1,22 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import { Table, TableHeader, TableRow, ToggleBtn } from 'Components';
 import styles from 'Components/MileageEtc/MileageEtc.module.css';
 
-const MileageEtc = ({ headTitle, menuTitle }) => {
-  const [value, setValue] = useState(false);
-
+	@@ -8,9 +8,8 @@ const MileageEtc = ({ headTitle, menuTitle }) => {
   const changeHandler = useCallback(
     state => {
       localStorage.setItem(menuTitle === '마일리지 적립' ? 'mileage' : 'etc', state);
+      setValue(state);
     },
-    [menuTitle]
+    [value]
   );
 
   useEffect(() => {
-    setValue(localStorage.getItem(menuTitle === '마일리지 적립' ? 'mileage' : 'etc'));
-  }, []);
-
-  return (
+	@@ -21,7 +20,7 @@ const MileageEtc = ({ headTitle, menuTitle }) => {
     <Table className={styles.table}>
       <TableHeader className={styles.headTitle}>{headTitle}</TableHeader>
       <TableRow label={menuTitle} className={styles.menuTitle}>
-        <ToggleBtn menuTitle={menuTitle} changeHandler={mileageEtcHandler} />
+        <ToggleBtn menuTitle={menuTitle} changeHandler={changeHandler} toggleValue={value} />
       </TableRow>
     </Table>
   );
-};
-
-export default MileageEtc;
