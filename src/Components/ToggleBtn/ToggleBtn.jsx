981e604c25d7ef19644e2684c5402bbce22691d8
pctmustang <85476908+leechoiswim1@users.noreply.github.com>
2022-01-29 06:44:44 +0900

    Modify - 토글버튼 데이터 연결 해제 및 인풋 네임 추가

--- src/Components/ToggleBtn/ToggleBtn.jsx
+++ src/Components/ToggleBtn/ToggleBtn.jsx
@@ -1,18 +1,9 @@
 import React from 'react';
 import styles from './ToggleBtn.module.css';
-const ToggleBtn = ({ menuTitle, changeHandler, toggleValue }) => {
+const ToggleBtn = ({ menuTitle }) => {
   return (
     <label htmlFor={menuTitle} class={styles.switchBtn}>
-      {toggleValue === 'true' ? (
-        <input
-          id={menuTitle}
-          type="checkbox"
-          checked
-          onChange={e => changeHandler(e.target.checked)}
-        />
-      ) : (
-        <input id={menuTitle} type="checkbox" onChange={e => changeHandler(e.target.checked)} />
-      )}
+      <input name={`${menuTitle}--${menuTitle}`} id={menuTitle} type="checkbox" />
       <span class={styles.onOffSwitch}></span>
     </label>
   );
