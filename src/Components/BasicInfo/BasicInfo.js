cee26c2e28674e6f08ed2ee80d8b452f43397329
yeongjong <tohero21@naver.com>
2022-01-28 18:10:01 +0900

    Merge upstream

--- src/Components/BasicInfo/BasicInfo.js
+++ src/Components/BasicInfo/BasicInfo.js
@@ -4,6 +4,8 @@ import styles from './BasicInfo.module.css';
 import { categories as originCategory } from 'data/basicInfo';
 import Card from './Card/Card';
 import Grid from 'Components/Grid/Grid';
+import Button from './Button/Button';
+import { withCurlyBraces } from 'utils';
 
 export default function BasicInfo() {
   const [categories, setCheckedCategories] = useState([...originCategory]);
@@ -42,18 +44,19 @@ export default function BasicInfo() {
                 {categories
                   .filter(({ checked }) => checked)
                   .map(({ id, category, checked }) => {
-                    return <CheckBox key={id} category={category} checked={checked} />;
+                    // return <CheckBox key={id} category={category} checked={checked} />;
+                    return <Button>{withCurlyBraces(category)}</Button>;
                   })}
               </ul>
             </Card>
           </Grid>
         </Grid>
       </TableRow>
-      <TableRow label="필터 태그" className={styles.basicInfo}>
+      {/* <TableRow label="필터 태그" className={styles.basicInfo}>
         temp value
       </TableRow>
       <TableRow>temp value</TableRow>
-      <TableRow>temp value</TableRow>
+      <TableRow>temp value</TableRow> */}
     </Table>
   );
 }
