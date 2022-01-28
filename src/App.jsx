ea2688a11d7637c05cd0e2c52a168ad2323f4829
hyun <vi2920va@gmail.com>
2022-01-28 16:57:00 +0900

    Add - ProductintroImg, BuyerRecommendImg 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
-import ProductOption from 'Components/ProductOption/ProductOption';
+import { classNames } from 'utils';
+import { BasicInfo, Grid, ProductOption } from 'Components';
+import styles from './App.module.css';
+import ProductIntroImg from './Components/ProductIntroImg/ProductIntroImg';
+import BuyerRecommendImg from './Components/BuyerRecommendImg/BuyerRecommendImg';
 
 const App = () => {
   return (
     <main className={styles.main}>
+      {/* conaiter */}
       <Grid container center>
         <Grid className={styles.menu} item>
           <nav>
@@ -18,6 +23,12 @@ const App = () => {
           </Grid>
           <Grid item>
             <ProductOption />
+          </Grid>
+          <Grid item container>
+            <ProductIntroImg />
+          </Grid>
+          <Grid item container>
+            <BuyerRecommendImg />
           </Grid>
         </Grid>
       </Grid>
