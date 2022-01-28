8025b835de9e9ea3cf67dd6390c2db7f448b2590
b41-41 <41@b41.kr>
2022-01-29 02:57:18 +0900

    Add - 노출 기한에 따른 미노출 자동 선택 기능
    - 노출 시작 날짜가 오늘 날짜보다 전의 값을 선택했을 때 미노출로 체크
    - 노출 종료 기한이 시작 날짜보다 전일 때 미노출로 체크
    - DatePicker 디자인 수정

--- src/Components/FormDatePicker/FormDatePicker.jsx
+++ src/Components/FormDatePicker/FormDatePicker.jsx
@@ -1,20 +1,38 @@
 import React, { useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import styles from "./FormDatePicker.module.css";
 
 const FormDatePicker = ({changeHandler}) => {
-  const [isDate, setIsDate] = useState(new Date());
-
+  const [isDate, setIsDate] = useState(null);
+  const [message, setMessage] = useState(false);
+  
   return (
-    <DatePicker 
-      selected={isDate} 
+    <>
+    <div className={styles.datePickerWrapper}>
+
+      <label className={styles.label}>
+      <DatePicker 
+      className={styles.selectDate}
+      selected={ isDate }
       showTimeSelect
       dateFormat="yyyy.MM.dd HH:mm"
+      placeholderText="YYYY.MM.DD YY:MM"
+      timeInputLabel="Time:"
       onChange={(date) => {
         setIsDate(date)
         changeHandler(date)
       }} 
-    />
+      />
+      <span className={styles.i}>
+        <i class="fas fa-chevron-down" />
+      </span>
+    </label>
+
+    </div>
+
+    
+    </>
   );
 };
 
